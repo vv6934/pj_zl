c89dfb7cd7be86c264460d1b4e9be39d6fff0b5b
vv6934 <97269754+vv6934@users.noreply.github.com>
2022-05-12 13:13:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,3 @@
-# Copyright 2020 Lorna Authors. All Rights Reserved.
-# Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-
-"""
-Evaluate on CIFAR. Note that at the moment, training is not implemented
-that being said, evaluation is working.
-"""
 import argparse
 import os
 import random
